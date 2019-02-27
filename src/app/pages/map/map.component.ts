50d5bfa7d7977c4b9387157daa4e2281854190cc
Ace.17 <simbulanja.17@gmail.com>
2019-02-27 17:18:06 +0800

    update

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/services/app.service';
+
+import {
+  Platform,
+  LoadingController
+} from '@ionic/angular';
+
 import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator/ngx';
 
 
@@ -11,18 +17,30 @@ import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-na
 export class MapComponent implements OnInit {
 
 
+  apps: string[] = [];
+
   loading: any;
   destination: number[];
   options: LaunchNavigatorOptions = {};
 
   constructor(
     public a: AppService,
+    private platform: Platform,
+    public loadingCtrl: LoadingController,
     private launchNavigator: LaunchNavigator
   ) {
     this.destination = [a.mapLat, a.mapLng];
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.platform.ready();
+    setTimeout(() => {
+      this.init();
+    }, 300);
+  }
+
+  init() {
+
   }
 
   navigate() {
