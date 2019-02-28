1d87273c637b3c2258cc57ab6c964ea9a33373d9
Ace.17 <simbulanja.17@gmail.com>
2019-02-28 15:22:08 +0800

    update for navigation

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { AppService } from 'src/app/services/app.service';
 import { Platform } from '@ionic/angular';
 import {
@@ -9,26 +9,23 @@ import {
 } from '@ionic-native/google-maps';
 import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator/ngx';
 
-
-
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit, AfterViewInit {
+export class MapComponent implements OnInit {
 
   @ViewChild('map_canvas') map_canvas: ElementRef;
   map: GoogleMap;
   loading: any;
-
-  apps = [];
 
   destination: number[];
   options: LaunchNavigatorOptions = {
     // app: will be user selection if waze and google maps are not available
     // transportMode: will be `driving` in defauld if not specified
   };
+  installedNavigationApps: Array<string> = [];
 
 
   constructor(
@@ -42,42 +39,51 @@ export class MapComponent implements OnInit, AfterViewInit {
   async ngOnInit() {
     await this.platform.ready();
     setTimeout(() => {
-      this.init();
-    }, 300);
+      this.loadMap();
+      console.log('OnInit');
+    }, 300); // timeout is necessary here.
   }
 
-
-  async ngAfterViewInit() {
-    await this.platform.ready();
-    setTimeout(() => this.loadMap(), 300); // timeout is necessary here.
-  }
-
-  init() {
+  loadMap() {
     /**
-     * returns installed navigation app from the user's device
+     * @description
+     *  `launchNavigator.availableApps` returns a list of supported apps for the current user's device with a boolean value
+     *    determining if it is installed or not.
+     *
+     * @return format
+     *  {
+     *    'google_maps' : true,
+     *    'waze' : false,
+     *     ...
+     *  }
+     *
+     * @note
+     *  - if a user's phone doesn't have any navigation app installed, falling back to user_select will not work
+     *      and the app will simply not show the selection box for navigation apps (because there is none).
+     *
+     *  - if that's the case, then this will also check if navigation apps supported by the `launch navigator` plugin is installed,
+     *      and if there's none installed from the list, we just simply notify the user to install a navigation app to continue.
+     *
+     * for the list of supported apps for android and iOs :
+     * @see https://github.com/dpa99c/phonegap-launch-navigator#app
      */
-    this.launchNavigator.availableApps().then(res => {
-      Object.keys(res).map((key) => {
-        /**
-         * check if waze is available then options.app will be waze if not, google maps
-         */
-        if (res[key]) {
-          if (key === this.launchNavigator.APP.WAZE) {
-            this.options.app = this.launchNavigator.APP.WAZE;
-          } else if (key === this.launchNavigator.APP.GOOGLE_MAPS) {
+    this.launchNavigator.availableApps().then(apps => {
+      Object.keys(apps).forEach(app => {
+        if (apps[app]) {
+          this.installedNavigationApps.push(app);
+
+          if (app === this.launchNavigator.APP.GOOGLE_MAPS) {
             this.options.app = this.launchNavigator.APP.GOOGLE_MAPS;
+          } else if (app === this.launchNavigator.APP.WAZE) {
+            this.options.app = this.launchNavigator.APP.WAZE;
           } else {
-            this.options.app = '';
+            this.options.app = this.launchNavigator.APP.USER_SELECT;
           }
         }
       });
-      console.log(this.options);
     });
-  }
 
-
-  loadMap() {
-    // console.log('canvas: ', this.map_canvas);
+    // plot map to canvas
     this.map = GoogleMaps.create(this.map_canvas.nativeElement, {
       camera: {
         target: {
@@ -89,7 +95,7 @@ export class MapComponent implements OnInit, AfterViewInit {
       }
     });
 
-    // add a marker
+    // add a marker to our destination
     const marker: Marker = this.map.addMarkerSync({
       title: this.a.t(this.a.settings.mapTitle),
       snippet: this.a.t(this.a.settings.mapSnippet),
@@ -100,12 +106,17 @@ export class MapComponent implements OnInit, AfterViewInit {
       animation: GoogleMapsAnimation.BOUNCE
     });
 
+    // show marker info
     marker.showInfoWindow();
   }
 
   onClickDirections() {
-    // for testing purposes
-    // this.options.app = '';
+
+    console.log(this.installedNavigationApps, this.options);
+
+    if (!this.installedNavigationApps.length) {
+      return alert('Please install a navigation app to continue');
+    }
 
     this.launchNavigator.navigate(this.destination, this.options)
       .then(
@@ -114,5 +125,17 @@ export class MapComponent implements OnInit, AfterViewInit {
       );
   }
 
+  /**
+   * tests
+   *
+   * Android :
+   *  5.1 - done, working
+   *    @todo - ERROR `net::ERR_CACHE_MISS` (gallery tab)
+   *  7.0 - done, working
+   *
+   * iOs :
+   *  N/A
+   *
+   */
 }
 
