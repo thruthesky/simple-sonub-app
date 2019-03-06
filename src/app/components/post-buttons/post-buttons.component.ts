488e45372e0c9df836a46cb26160d470a2fd4c94
Ace.17 <simbulanja.17@gmail.com>
2019-03-06 14:26:32 +0800

    app redirect update

--- src/app/components/post-buttons/post-buttons.component.ts
+++ src/app/components/post-buttons/post-buttons.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post, Comment } from 'modules/ng-simplest/simplest.interface';
 import { AppService } from 'src/app/services/app.service';
+import { AppSettingForum } from 'src/app/services/interfaces';
 
 @Component({
     selector: 'app-post-buttons',
@@ -10,40 +11,27 @@ import { AppService } from 'src/app/services/app.service';
 export class PostButtonsComponent implements OnInit {
 
     @Input() parent: Post & Comment;
+    @Input() forum: AppSettingForum;
     constructor(
         public a: AppService
     ) {
     }
 
     ngOnInit() {
-    }
-
-    /**
-     * returns this.parent type
-     *
-     * @return 'post' | 'comment'
-     */
-    get type(): string {
-        if (this.parent.idx_parent === '0') {
-            return 'post';
-        } else {
-            return 'comment';
+        if (this.parent) {
+            console.log(this.parent);
         }
     }
-
-    /**
-     * return forum type - 'sonub' | 'philgo'
-     */
-    // get forum(): string {
-    //     return this.a.settings.site.forum.type;
-    // }
-
 
     /**
      * return current host url
      */
     get hostUrl(): string {
         return this.a.settings.site.url;
+    }
+
+    get forumType(): string {
+        return this.forum.type;
     }
 
     get mine(): boolean {
@@ -53,26 +41,63 @@ export class PostButtonsComponent implements OnInit {
         return false;
     }
 
+    get postIdx() {
+        if (this.parent.idx_root !== '0') {
+            return this.parent.idx_root;
+        } else if (this.parent.idx_parent !== '0') {
+            return this.parent.idx_parent;
+        } else {
+            return this.parent.idx;
+        }
+    }
+
     /**
      * @param action like | dislike | create | update | delete
      * @param type post | comment
      * @param idx category_idx | post_idx | comment_idx
      */
     openExternal(action: string) {
-        let idx = this.parent.idx;
-        /**
-         * if comment, append idx_parent or root
-         */
-        if (this.type === 'comment') {
-            if (this.parent.idx_root === this.parent.idx_parent && action === 'create') {
-                idx = `${this.parent.idx_parent}.${idx}`;
-                action = 'post-reply';
+        let redirect_url = 'r=';
+
+        if (this.forumType === 'sonub') {
+            /**
+             * @format
+             * - update post `post/:editor/:action/:post_id`
+             *      [:editor] may vary from `simple-editor`, `text-editor` and `ckeditor-editor`
+             *
+             * - view post `post/:action/:post_idx/:title`
+             */
+            if (action === 'update') {
+                redirect_url += `post/${this.parent.editor}/${action}/${this.postIdx}`;
             } else {
-                idx = `${this.parent.idx_root}.${idx}`;
-                action = 'comment-reply';
+                redirect_url += `post/${action}/${this.postIdx}/${this.parent.title}`;
+            }
+        } else {
+            /**
+             * @format
+             * - update post `philgo/update/:category_id.sub_cat/:post_idx`;
+             *      [:category_id] is appended by it's [:sub_category] if there is any
+             *
+             * - view post `philgo/view/:category_id/:post_idx`
+             *
+             * @note
+             * - this need to be tests for philgo posts
+             */
+            if (action === 'update') {
+                redirect_url += `philgo/${action}/${this.parent.idx_category}/${this.postIdx}`;
+            } else {
+                redirect_url += `philgo/${action}/${this.parent.idx_category}/${this.postIdx}`;
             }
         }
-        this.a.openExternal(`${this.hostUrl}/login-first/LoginFirst/Please%20Login%20first/You%20can%20login%20or%20register/help`);
+
+        // this.a.openExternal(
+        //     this.hostUrl
+        //     + '/login-first/LoginFirst/Please%20Login%20first/You%20can%20login%20or%20register?'
+        //     + redirect_url
+        // );
+
+        // test
+        this.a.openExternal(`https://user.sonub.com:8443/login-first/LoginFirst/PleaseLoginFirst/LoginOrRegister?${redirect_url}`);
     }
 }
 
