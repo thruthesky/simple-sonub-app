08ede7b03ee808dcc01c02288fbdbc7ac206b8b6
Ace.17 <simbulanja.17@gmail.com>
2019-03-19 21:06:41 +0800

    home page

--- src/app/pages/home/evieco/evieco-home/evieco-home.locale.ts
+++ src/app/pages/home/evieco/evieco-home/evieco-home.locale.ts
@@ -1,24 +1,40 @@
 export const text = {
     title: {
-        title: { en: '2019 Best Restaurant', ko: '에비코 Evie&Co 반영구 화장', ch: '默认应用', jp: 'デフォルトアプリ' },
-        subtitle: { en: 'Subtitle of home', ko: '당신의 하루를 바꾸는', ch: '家的副标题', jp: '家の字幕' },
+        title: {
+            en: 'Evie & Co. Permanent makeup & Eyelash extension',
+            ko: '에비코 Evie&Co 반영구 화장',
+            ch: 'Evie＆Co。永久化妆和睫毛延长',
+            jp: 'Evie＆Co.パーマネントメイクアップ＆まつげエクステ'
+        },
+        subtitle: {
+            en: 'Subtitle of home',
+            ko: '당신의 하루를 바꾸는',
+            ch: '家的副标题',
+            jp: '家の字幕'
+        },
         content: {
-            en: `Helmed by Rei Masuda, Jiro Ono's 36-year-old protege and master of seasoning, Sushi Wakon at The Four Seasons Kyoto promises diners a "quintessential Ginza sushi experience."`,
-            ko: `
-눈과 함께 눈썹은 사람의 인상을 좌우하는 큰 요소 중 하나입니다.
-눈썹 화장을 하는 이유는 예뻐 보이기 위해서거나 좋은 인상을 주기 위해서이죠.
+            en: `
+            Eyebrows are one of the major factors that influence the impression of a person.
+            The reason for eyebrow makeup is to look pretty or give a good impression.`,
 
-`,
-            ch: `日本四季酒店的寿司Wakon由36岁的Jiro Masuda和大调调味品主持人Rei Masuda主持，为食客提供“典型的银座寿司体验”。`,
-            jp: `小野次郎さんの36歳の抗議者で調味料の達人である増田玲氏の協力のもと、フォーシーズンズ京都のSushi Wakonは「典型的な銀座寿司体験」をお約束します。`
+            ko: `
+            눈과 함께 눈썹은 사람의 인상을 좌우하는 큰 요소 중 하나입니다.
+            눈썹 화장을 하는 이유는 예뻐 보이기 위해서거나 좋은 인상을 주기 위해서이죠.`,
+
+            ch: `
+            眉毛是影响人的印象的主要因素之一。
+            眉妆的原因是看起来很漂亮或给人留下好印象。`,
+
+            jp: `目と眉毛は人の印象を左右する大きな要素の一つです。
+            眉毛化粧をする理由は、きれいに見えるためか、良い印象を与えるためにです。`
         },
         contentMore: {
 
             en: `
-            <div class="mt-3">드라마나 영화를 보면 모든 남녀가 눈썹 화장을 하는 이유이기도 한데
-            이외에도 눈썹 화장을 해야하는 이유는 다양합니다. 개인에게 가장 잘 어울리는 눈썹을 찾지 못하고 계시거나 나이가 들어 눈썹 숱이 적어진 경우 또는 원래 부터 눈썹이 얕거나 빈곳이 있는 경우 등.
-            눈썹 반영구 화장은 눈썹 문신이라고도 하며 한번 시술을 하면 최소 몇 년간 유지가 됩니다.</div>
-            <div class="mt-3">에비코 Evie&Co 에서는 숙련된 한국인이 눈썹 반영구 화장을 시술하여 여러분들의 얼굴형에 맞는 가장 알맞은 눈썹을 찾아 드릴 것입니다.</div>`,
+            <div class="mt-3">The drama and movie show why all men and women make eyebrows.
+            There are many reasons for eyebrow makeup. If you do not find the best looking eyebrow that will suit your need, or if your eyebrows are thinner due to age, or if your eyebrows are shallow or empty from the beginning.
+            Eyebrow semi-permanent makeup is also called eyebrow tattoo, and once done, it will last for at least several years.</div>
+            <div class="mt-3">Evie & Co will treat your eyebrows with a semi-permanent eyebrow makeup by skilled Korean people and find the right eyebrow for your face type.</div>`,
 
             ko: `
             <div class="mt-3">드라마나 영화를 보면 모든 남녀가 눈썹 화장을 하는 이유이기도 한데
@@ -27,16 +43,16 @@ export const text = {
             <div class="mt-3">에비코 Evie&Co 에서는 숙련된 한국인이 눈썹 반영구 화장을 시술하여 여러분들의 얼굴형에 맞는 가장 알맞은 눈썹을 찾아 드릴 것입니다.</div>`,
 
             ch: `
-            <div class="mt-3">드라마나 영화를 보면 모든 남녀가 눈썹 화장을 하는 이유이기도 한데
-            이외에도 눈썹 화장을 해야하는 이유는 다양합니다. 개인에게 가장 잘 어울리는 눈썹을 찾지 못하고 계시거나 나이가 들어 눈썹 숱이 적어진 경우 또는 원래 부터 눈썹이 얕거나 빈곳이 있는 경우 등.
-            눈썹 반영구 화장은 눈썹 문신이라고도 하며 한번 시술을 하면 최소 몇 년간 유지가 됩니다.</div>
-            <div class="mt-3">에비코 Evie&Co 에서는 숙련된 한국인이 눈썹 반영구 화장을 시술하여 여러분들의 얼굴형에 맞는 가장 알맞은 눈썹을 찾아 드릴 것입니다.</div>`,
+            <div class="mt-3">电视剧和电影节目展示了为什么所有男人和女人都会眉毛。
+            眉妆有很多原因。 如果你没有为你的个人找到最好看的眉毛，或者你的眉毛由于年龄而变瘦，或者你的眉毛从一开始就是浅的或空的。
+            眉毛半永久性化妆也称为眉纹纹，一旦完成，它将持续至少数年。</div>
+            <div class="mt-3"> Ebco Evie＆Co将为熟练的韩国人提供半永久性的眉妆，并为您的脸型找到合适的眉毛。</div>`,
 
             jp: `
-            <div class="mt-3">드라마나 영화를 보면 모든 남녀가 눈썹 화장을 하는 이유이기도 한데
-            이외에도 눈썹 화장을 해야하는 이유는 다양합니다. 개인에게 가장 잘 어울리는 눈썹을 찾지 못하고 계시거나 나이가 들어 눈썹 숱이 적어진 경우 또는 원래 부터 눈썹이 얕거나 빈곳이 있는 경우 등.
-            눈썹 반영구 화장은 눈썹 문신이라고도 하며 한번 시술을 하면 최소 몇 년간 유지가 됩니다.</div>
-            <div class="mt-3">에비코 Evie&Co 에서는 숙련된 한국인이 눈썹 반영구 화장을 시술하여 여러분들의 얼굴형에 맞는 가장 알맞은 눈썹을 찾아 드릴 것입니다.</div>`
+            <div class="mt-3">ドラマや映画を見ると、すべての男女が眉メイクをする理由でもあるが
+            他にも眉メイクをする理由はさまざまです。個人に最もよく似合う眉が見つからず、明らかにしたり、年齢が含まれて眉毛が少なくなった場合、または、もともとの眉が浅くしたり、ビンゴトがある場合など。
+            眉毛半永久化粧は眉毛の入れ墨とも呼ばれ、一度施術をすると、少なくとも数年間維持がされます。</ div>
+            <div class="mt-3">海老名鼻Evie＆Coは、熟練した韓国人が眉半永久化粧を施術して皆さんの顔型に合わせて最適な眉を見つけいたします。</div>`
 
         }
     }
@@ -44,28 +60,43 @@ export const text = {
 };
 
 export const imageTextTitles = [
-    { en: 'Fish & Tomato', ko: '남자눈썹', ch: 'Fish & Tomato', jp: 'Fish & Tomato' },
-    { en: 'Label', ko: '속눈썹연장', ch: '标签', jp: 'ラベル' },
-    { en: 'Hamberger', ko: '반영구화장', ch: 'Hamberger', jp: 'Hamberger' },
-    { en: 'Chiken', ko: '헤어라인', ch: 'Chiken', jp: 'Chiken' },
+    { en: 'Eyelash perm', ko: '남자눈썹', ch: '男眉', jp: '男の眉毛' },
+    { en: 'Eyelash extension', ko: '속눈썹연장', ch: '睫毛延长', jp: 'まつげの拡張' },
+    { en: 'Micro blading', ko: '반영구화장', ch: '半永久性化妆', jp: '半永久化粧' },
+    { en: 'Hairline', ko: '헤어 라인', ch: '发际线', jp: '生え際' },
 ];
 
 
 export const iconTexts = [
-    {en: 'This is first icon text.', ko: '첫 번째 아이콘의 텍스트입니다.', ch: '这是第一个图标文字。', jp: 'これが最初のアイコンテキストです。'},
-    {en: 'This is second icon text.', ko: '두 번째 아이콘의 텍스트입니다.', ch: '这是第一个第二个文本。', jp: 'これは最初の2番目のテキストです。'},
-    {en: 'This is third icon text.', ko: '세 번째 아이콘의 텍스트입니다.', ch: '这是第一个第三个文本。', jp: 'これが最初の3分の1のテキストです。'},
-    {en: 'This is fourth icon text.', ko: '네 번째 아이콘의 텍스트입니다.', ch: '这是第四个文本。', jp: 'これは最初の4番目のテキストです。'}
-  ];
+    {
+        en: 'Eyelash perm or eyelash permanent wave, and can also refer to permanent relaxer that straightens the hair is a cosmetics procedure performed to flip up eyelashes using hair perming technology.',
+        ko: '속눈썹 파마 또는 속눈썹 영구 파, 머리카락을 곧게 펴는 영구적 인 완화제는 헤어 퍼머 기술을 사용하여 속눈썹을 뒤집어서 수행하는 화장품 절차입니다.',
+        ch: '睫毛烫发或睫毛永久性波浪，也可以指使头发拉直的永久性松弛剂是使用头发烫发技术翻转睫毛的化妆品程序。',
+        jp: 'まつげのパーマやまつげのパーマネントウェーブ、そしてまた髪をまっすぐにするパーマネントリラクサーを指すことができますヘアパーマ技術を使用してまつげを裏返しにするために行われる化粧品の手順です。'
+    },
+    {
+        en: 'Eyelash extensions are used to enhance the length, curliness, fullness, and thickness of natural eyelashes.',
+        ko: '속눈썹 연장은 자연 속눈썹의 길이, 컬링, 충만 및 두께를 향상시키는 데 사용됩니다.',
+        ch: '睫毛扩展用于增强自然睫毛的长度，卷曲度，丰满度和厚度。',
+        jp: 'まつげエクステは、天然まつげの長さ、丸み、丸み、太さを引き立てます。'
+    },
+    {
+        en: 'Microblading is a tattooing technique in which a small handheld tool made of several tiny needles is used to add semi-permanent pigment to the skin, and is used on eyebrows to create, enhance or reshape their appearance in terms of both shape and colour.',
+        ko: '마이크로 블레이드는 피부에 반영구적 인 안료를 첨가하기 위해 몇 개의 작은 바늘로 만들어진 작은 휴대용 도구가 사용되는 문신 기술로, 모양과 색상면에서 외관을 개선하거나 모양을 변경하기 위해 눈썹에 사용됩니다.',
+        ch: 'Microblading是一种纹身技术，其中使用由几根细针制成的小型手持工具向皮肤添加半永久性颜料，并且用于眉毛以在形状和颜色方面创建，增强或重塑它们的外观。',
+        jp: 'マイクロブレードは、いくつかの小さな針でできた小さな手の道具を使って半永久的な色素を肌に加えたり、眉毛の上で形や色の点で外観を作り出したり、高めたり、形を変えたりするためのものです。'
+    },
+    { en: 'This is fourth icon text.', ko: '네 번째 아이콘의 텍스트입니다.', ch: '这是第四个文本。', jp: 'これは最初の4番目のテキストです。' }
+];
 
 
 export const cards = [
     {
         title: {
-            en: 'Card Title 1',
-            ko: '카드 제목 1',
-            ch: '卡片标题 1',
-            jp: 'カードのタイトル 1'
+            en: 'Our Vision and Mission',
+            ko: '우리의 비전과 사명',
+            ch: '我们的愿景和使命',
+            jp: '私たちのビジョンとミッション'
         },
         subtitle: {
             en: 'Card Subtitle',
@@ -74,10 +105,10 @@ export const cards = [
             jp: 'カード字幕'
         },
         name: {
-            en: 'Name Deaut',
-            ko: '기본 이름',
-            ch: 'Default Name',
-            jp: 'Default Name'
+            en: 'Evie & Co',
+            ko: 'Evie & Co',
+            ch: 'Evie & Co',
+            jp: 'Evie & Co'
         },
         date: {
             en: '0956 871 2485',
@@ -86,25 +117,29 @@ export const cards = [
             jp: '0956 871 2485'
         },
         content: {
-            en: `Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.`,
-            ko: `자연의 마음과 가깝게 지내십시오. 한동안 잠시 쉬고 산을 오르거나 숲에서 일주일을 보내십시오. 내용 1...`,
-            ch: `靠近大自然的心脏......偶尔打破一下，爬山或在树林里度过一个星期。 洗净你的精神。`,
-            jp: `自然の心に近づけないでください…そして時々、立ち去り、山に登ったり、森の中で一週間過ごしたりしましょう。 あなたの精神をきれいに洗ってください。`
+            en: `<b>Vision</b>: To become a well-known provider of good and quality service when it comes to beauty.`,
+            ko: `<b> 비전 </b> : 아름다움에 관해서는 양질의 서비스를 제공하는 유명한 제공자가되는 것.`,
+            ch: `<b>愿景</ b>：在美容方面成为知名的优质服务提供商。`,
+            jp: `<b>ビジョン</b>：美しさに関しては、よく知られた質の高いサービスを提供することです。`
         },
         contentMore: {
-          en: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
-          ko: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
-          ch: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
-          jp: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
+            en: `
+            <div class="mt-3"><b>Mission</b> - To serve our clients with an excellent customer care and satisfaction. And to promote health and wellness inside and outside the community.</div>`,
+
+            ko: `<div class="mt-3"><b>사명</b> - 우수한 고객 관리와 만족을 고객에게 제공합니다. 지역 사회 안팎에서 건강과 웰빙을 증진합니다.</div>`,
+
+            ch: `<div class="mt-3"><b>使命</b> - 以优质的客户服务和满意度为客户服务。 并促进社区内外的健康和健康。</div>`,
+
+            jp: `<div class="mt-3"><b>ミッション</b> - 優れたカスタマーケアと満足度をお客様に提供します。 そして、コミュニティの内外で健康と健康を増進すること。</div>`,
         }
     },
 
     {
         title: {
-            en: 'Card Title 2',
-            ko: '카드 제목 2',
-            ch: '卡片标题 2',
-            jp: 'カードのタイトル 2'
+            en: 'Certified Technicians Experts',
+            ko: '공인 기술자 전문가',
+            ch: '认证技师专家',
+            jp: '認定技術者エキスパート'
         },
         subtitle: {
             en: 'Card Subtitle',
@@ -113,10 +148,10 @@ export const cards = [
             jp: 'カード字幕'
         },
         name: {
-            en: 'Name Deaut',
-            ko: '기본 이름',
-            ch: 'Default Name',
-            jp: 'Default Name'
+            en: 'Evie & Co',
+            ko: 'Evie & Co',
+            ch: 'Evie & Co',
+            jp: 'Evie & Co'
         },
         date: {
             en: '0956 871 2485',
@@ -125,25 +160,33 @@ export const cards = [
             jp: '0956 871 2485'
         },
         content: {
-            en: `Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.`,
-            ko: `자연의 마음과 가깝게 지내십시오. 한동안 잠시 쉬고 산을 오르거나 숲에서 일주일을 보내십시오. 정신을 깨끗이 씻으십시오.`,
-            ch: `靠近大自然的心脏......偶尔打破一下，爬山或在树林里度过一个星期。 洗净你的精神。`,
-            jp: `自然の心に近づけないでください…そして時々、立ち去り、山に登ったり、森の中で一週間過ごしたりしましょう。 あなたの精神をきれいに洗ってください。`
+            en: `Our team members are trained to create bespoke cuts that fit the taste and lifestyle of each of our client.`,
+            ko: `우리 팀원은 각 고객의 취향과 라이프 스타일에 맞는 맞춤형 커팅을 제작할 수 있도록 교육을 받았습니다.`,
+            ch: `我们的团队成员经过培训，可以根据我们每个客户的口味和生活方式进行定制切割。`,
+            jp: `私達のチームメンバーは私達の顧客のそれぞれの好みそして生活様式に合う特注カットを作成するために訓練されます。`
         },
         contentMore: {
-          en: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
-          ko: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
-          ch: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
-          jp: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
+            en: `
+            <div class="mt-3">Each stylist or technician has an instinctive understanding of the science behind eyebrow, complemented by an understanding of fashion and trends.
+            Our trademark is our unique, visual, modern and natural for what works, combined with a great sense of style and extensive experience.</div>`,
+
+            ko: `<div class="mt-3">각 스타일리스트 또는 기술자는 패션과 트렌드에 대한 이해로 보완 된 눈썹 뒤에있는 과학에 대해 본능적으로 이해합니다.
+            우리 상표는 독특하고 시각적이며 모던하고 자연스럽고 스타일리쉬하고 폭 넓은 경험을 제공합니다.</div>`,
+
+            ch: `<div class="mt-3">每位造型师或技师都对眉毛背后的科学有着本能的理解，并辅以对时尚和潮流的理解。
+            我们的商标是我们独特，视觉，现代和自然的作品，结合了极具时尚感和丰富的经验。</div>`,
+
+            jp: `<div class="mt-3">それぞれのスタイリストや技術者は、眉毛の背後にある科学についての本能的な理解を持っています。そして、ファッションやトレンドについての理解によって補完されています。
+            私たちの商標は、優れたスタイルと豊富な経験を兼ね備えた、当社独自のビジュアル、モダン、そして自然なものです。</div>`,
         }
     },
 
     {
         title: {
-            en: 'Card Title 3',
-            ko: '카드 제목 3',
-            ch: '卡片标题 3',
-            jp: 'カードのタイトル 3'
+            en: 'Testimonials',
+            ko: '회원 평가',
+            ch: '褒奖',
+            jp: 'お客様の声'
         },
         subtitle: {
             en: 'Card Subtitle',
@@ -152,10 +195,10 @@ export const cards = [
             jp: 'カード字幕'
         },
         name: {
-            en: 'Name Deaut',
-            ko: '기본 이름',
-            ch: 'Default Name',
-            jp: 'Default Name'
+            en: 'Evie & Co',
+            ko: 'Evie & Co',
+            ch: 'Evie & Co',
+            jp: 'Evie & Co'
         },
         date: {
             en: '0956 871 2485',
@@ -164,16 +207,42 @@ export const cards = [
             jp: '0956 871 2485'
         },
         content: {
-            en: `Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.`,
-            ko: `자연의 마음과 가깝게 지내십시오. 한동안 잠시 쉬고 산을 오르거나 숲에서 일주일을 보내십시오. 정신을 깨끗이 씻으십시오.`,
-            ch: `靠近大自然的心脏......偶尔打破一下，爬山或在树林里度过一个星期。 洗净你的精神。`,
-            jp: `自然の心に近づけないでください…そして時々、立ち去り、山に登ったり、森の中で一週間過ごしたりしましょう。 あなたの精神をきれいに洗ってください。`
+            en: `Got my eyebrow tattoo and eyelash extension here with perm. Results were so gorgeous.
+            Satisfied! Didn't ruined my natural lashes. They stayed intact while extensions start falling off after few months.
+            Didn't left my lashes bald. Lol.<br /> - <i>shiki t</i>`,
+
+            ko: `내 눈썹 문신과 속눈썹 확장을 파마로 가져 왔습니다. 결과는 너무 화려했습니다.
+            만족! 내 자연 속눈썹을 망치지 않았어. 연장 기간은 몇 개월이 지나면 떨어지기 시작하면서 그대로 유지되었습니다.
+            내 속눈썹을 대머리 상태로 두지 않았습니까? Lol.<br /> - <i>shiki t</i>`,
+
+            ch: `烫发时，我的眉毛纹身和睫毛延长。 结果如此华丽。
+            满意！ 没有破坏我的天然睫毛。 他们保持完整，而扩展在几个月后开始下降。 没有留下我的睫毛秃头。
+            大声笑。<br /> - <i>shiki t</i>`,
+
+            jp: `ここにパーマと私の眉毛の入れ墨とまつげの拡張子を得た。 結果はとてもゴージャスでした。
+            満足！ 私の自然なまつ毛を台無しにしなかった。 エクステンションは数ヶ月後に減少し始めましたが、それらはそのまま残りました。
+            私のまつげをはげたままにしなかった。 笑。<br /> - <i>shiki t</i>`
         },
         contentMore: {
-          en: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
-          ko: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
-          ch: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
-          jp: `<div class="mt-3">This is more content. This is more content. This is more content. This is more content. This is more content. This is more content. This is more content.</div>`,
+            en: `It was my first time getting eyeliner tattoo in my life and I was super satisfied!!!
+            My make up artist (Ha Yeon) was very friendly and she checked up my condition and explained the procedure very well before starting, which made me to be emotionally comfortable.
+            I've been hearing that eyeliner tattoo really hurts; however, the one I got from Ha Yeon did not hurt as much as I expected!
+            I even fell asleep while getting it. Definitely gonna visit again for eyebrow tattoo soon!! :) Super recommended! <br />- <i>YeongHyeon K</i>`,
+
+            ko: `내 인생에서 아이 라이너 타투를 얻는 것은 처음이었고 나는 만족스러워했습니다 !!!
+            메이크업 아티스트 (하연)는 매우 친절했고 그녀는 내 상태를 확인하고 시작하기 전에 절차를 잘 설명하여 감정적으로 편안하게 만들었습니다.
+            나는 아이 라이너 문신이 정말 아파한다고 들었습니다. 그러나 내가 Ha 연에서 얻었던 것은 내가 예상했던 것만 큼 상처를 입지 않았다! 나는 그것을 얻는 동안 심지어 잠 들었다.
+            곧 눈썹 문신을 다시 방문 할 것입니다! :) 슈퍼 추천!<br />- <i>YeongHyeon K</i>`,
+
+            ch: `<div class="mt-3">这是我第一次在我的生活中得到眼线纹身，我非常满意！
+            我的化妆师（Ha Yeon）非常友好，她检查了我的病情并在开始前很好地解释了这个程序，这让我感觉很舒服。
+            我一直听说眼线纹身真疼！ 然而，我从Ha Yeon那里得到的那个并没有像我预期的那样受到伤害！ 我得到它时甚至睡着了。
+            绝对会再次访问眉毛纹身很快!! :) 超级推荐！ <br />- <i>YeongHyeon K</i>`,
+
+            jp: `私の人生でアイライナーのタトゥーを手に入れたのは今回が初めてで、とても満足していました。
+            私のメーキャップアーティスト（Ha Yeon）はとても親切で、彼女は私の体調をチェックし、始める前にその手順をとてもよく説明してくれました。
+            アイライナーのタトゥーが本当に痛いと聞いています。 しかし、私がハヨンから手に入れたものは私が思ったほど傷つくものではありませんでした！
+            それを取得しながら私も眠りに落ちた。 まもなくまた眉タトゥーのために再び訪れるつもりです！ :) 超おすすめ！<br />- <i>YeongHyeon K</i>`,
         }
     }
 ];
